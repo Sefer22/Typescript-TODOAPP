6e04cb0a16bc56be39f3e021fd4c835131d3da89
Sefer22 <sfrs87997@gmail.com>
2024-08-08 00:40:06 +0400

    developed

--- typescript-todoapp/src/components/Todo.tsx
+++ typescript-todoapp/src/components/Todo.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { IoMdRemoveCircleOutline } from "react-icons/io";
+import { FaCheck } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
 
 function Todo() {
   return (
-    <div>
-      
+   <div>
+     <div>
+      I'm first todo...
     </div>
+   </div>
   )
 }
 
