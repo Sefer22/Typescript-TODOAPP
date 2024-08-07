13275f73ce0f5ed03fc4b826afaa28a433026df0
Sefer22 <sfrs87997@gmail.com>
2024-08-08 00:33:40 +0400

    developed

--- typescript-todoapp/src/components/TodoCreate.tsx
+++ typescript-todoapp/src/components/TodoCreate.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function TodoCreate() {
   return (
-    
+
     <div className='todo-create'>
-        <input className='text' type="text" />
+        <input className='text' type="text" placeholder='Enter todo...' />
         <button>Create</button>
     </div>
   )
