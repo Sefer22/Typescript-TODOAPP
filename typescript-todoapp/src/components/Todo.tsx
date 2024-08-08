93163ffe2352afa2e1be08e6f9d33f46d0deb9e7
Sefer22 <sfrs87997@gmail.com>
2024-08-08 19:28:51 +0400

    developed

--- typescript-todoapp/src/components/Todo.tsx
+++ typescript-todoapp/src/components/Todo.tsx
@@ -3,6 +3,8 @@ import { IoMdRemoveCircleOutline } from "react-icons/io";
 import { FaCheck } from "react-icons/fa";
 import { FaEdit } from "react-icons/fa";
 import { TodoType } from '../types/Types';
+import { useDispatch } from 'react-redux';
+import { removeTodoById } from '../redux/todoSlice';
 
 interface TodoProps {
   todoProps:TodoType
@@ -10,17 +12,25 @@ interface TodoProps {
 
 function Todo({todoProps} :TodoProps) {
   const {id,content} = todoProps;
+
+  const dispatch = useDispatch();
+
+  const handleRemoveTodo = () =>{
+    dispatch(removeTodoById(id));
+  }
+
   return (
    <div style={{display:'flex',flexDirection:'row'
    ,alignItems:'center',justifyContent:'space-between',
    border:'1px solid lightgrey',padding:'16px',marginTop:'25px',
    borderRadius:'5px'}}>
+
      <div>
       {content}
     </div>
     <div style={{display:'flex',flexDirection:'row'
    ,alignItems:'center',justifyContent:'center'}}>
-        <div style={{marginRight:'5px'}} className='icons'><IoMdRemoveCircleOutline/></div>
+        <div onClick={handleRemoveTodo} style={{marginRight:'5px'}} className='icons'><IoMdRemoveCircleOutline /></div>
         <div className='icons'><FaCheck/></div>
     </div>
    </div>
