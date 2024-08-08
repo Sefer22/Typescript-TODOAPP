be1da04fd30999179c90d4b593e875c3622b2bc5
Sefer22 <sfrs87997@gmail.com>
2024-08-08 19:08:52 +0400

    developed

--- typescript-todoapp/src/components/Todo.tsx
+++ typescript-todoapp/src/components/Todo.tsx
@@ -2,15 +2,21 @@ import React from 'react'
 import { IoMdRemoveCircleOutline } from "react-icons/io";
 import { FaCheck } from "react-icons/fa";
 import { FaEdit } from "react-icons/fa";
+import { TodoType } from '../types/Types';
 
-function Todo() {
+interface TodoProps {
+  todoProps:TodoType
+}
+
+function Todo({todoProps} :TodoProps) {
+  const {id,content} = todoProps;
   return (
    <div style={{display:'flex',flexDirection:'row'
    ,alignItems:'center',justifyContent:'space-between',
    border:'1px solid lightgrey',padding:'16px',marginTop:'25px',
    borderRadius:'5px'}}>
      <div>
-      I'm first todo...
+      {content}
     </div>
     <div style={{display:'flex',flexDirection:'row'
    ,alignItems:'center',justifyContent:'center'}}>
