8ed0d846ab0c75667ccc00f50f24a561eb7dbf1b
Sefer22 <sfrs87997@gmail.com>
2024-08-08 18:28:08 +0400

    developed

--- typescript-todoapp/src/components/TodoCreate.tsx
+++ typescript-todoapp/src/components/TodoCreate.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 function TodoCreate() {
+  const dispatch = useDispatch();
+
+  const [newTodo,setNewTodo] = useState<string>('');
+
+  const handleCreateTodo =() =>{
+
+  }
+
   return (
 
     <div className='todo-create'>
-        <input className='text' type="text" placeholder='Enter todo...' />
-        <button>Create</button>
+        <input 
+        value={newTodo}
+        onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setNewTodo(e.target.value)}
+        className='text' type="text" placeholder='Enter todo...' />
+        <button onClick={handleCreateTodo}>Create</button>
     </div>
   )
 }
